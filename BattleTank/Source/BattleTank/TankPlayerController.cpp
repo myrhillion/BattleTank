0c212fe70e43a995208f83af3eb83e553a4ba5b3
Doug Taggart <doug.taggart@gmail.com>
2020-05-29 19:14:18 -0400

    Begin Aiming System

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,10 +3,7 @@
 
 #include "TankPlayerController.h"
 
-ATank* ATankPlayerController::GetControlledTank() const
-{
-    return Cast<ATank>(GetPawn());
-}
+
 
 void ATankPlayerController::BeginPlay()
 {
@@ -22,4 +19,38 @@ void ATankPlayerController::BeginPlay()
     {
         UE_LOG(LogTemp, Warning, TEXT("No Controlled Tank available."));
     }
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    // AimTowardsCrossHair();
+    // UE_LOG(LogTemp, Warning, TEXT("Ticking in TankPlayerController"));
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrossHair()
+{
+    if (!GetControlledTank()){return;}
+
+
+    FVector HitLocation; // Out Parameter
+    if (GetSightRayHitLocation(HitLocation)) // is going to ray-trace later
+    {
+        // Get World Location through crosshair (line trace)
+        // If it hits the landscape
+        // Tell controlled tank to aim at this point
+        UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+        // TODO Tell controlled tank to aim at this point
+    }
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+    HitLocation = FVector(1.0);
+    return true;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,6 +16,13 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	virtual void Tick(float DeltaTime) override;
 	ATank* GetControlledTank() const;
 	void BeginPlay() override;
+
+private:
+	// Start the tank moving the barrel so that a shot would hit where 
+	// The crosshair intersects the world
+	void AimTowardsCrossHair();
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 };
