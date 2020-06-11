74ffc32309df3e049e95c3d62908dae69ab0ea55
Doug Taggart <doug.taggart@gmail.com>
2020-06-10 20:39:36 -0400

    healthbar completion, damage not registering yet though

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -12,12 +12,23 @@ ATank::ATank()
 	PrimaryActorTick.bCanEverTick = false;
 }
 
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;
+}
+
 float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
 {
 	int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount);
 	int32 DamageToApply = FMath::Clamp(DamagePoints, 0, CurrentHealth);
+	CurrentHealth -= DamageToApply;
+	if (CurrentHealth <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank died."));
+	}
+	
 	return DamageToApply;
 
-	UE_LOG(LogTemp, Warning, TEXT("DamageAmount: %f, DamageToApply: %i"), DamageAmount, DamageToApply);
+	
 }
 
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -13,10 +13,17 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-	ATank();
+	
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
+	// returns current health as percentage of starting health, between 0 and 1.
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
+
+	ATank();
+
 	// Sets default values for this pawn's properties
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
