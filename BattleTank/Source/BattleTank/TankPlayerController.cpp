3b11a1a72e5b6046cbe324342a92b50b6aaf18ba
Doug Taggart <doug.taggart@gmail.com>
2020-06-11 22:27:47 -0400

    minor changes fixed a visibility bug, added camera toggle

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -97,7 +97,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVec
         HitResult,
         StartLocation,
         EndLocation,
-        ECollisionChannel::ECC_Visibility)
+        ECollisionChannel::ECC_Camera)
         )
     {
         // set hit location
